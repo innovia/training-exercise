64cfa82764126fe19ea05b5d03433815fb889f42
Ami Mahloof <ami@lightricks.com>
2016-06-20 16:15:26 +0300

    fix fahrenheit function

--- 03-functions-and-loops/temperatures.py
+++ 03-functions-and-loops/temperatures.py
@@ -1,8 +1,7 @@
 # Copyright (c) 2016 Ami . All rights reserved
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit = float(celsius) * 9/5 + 32
-    return fahrenheit
+    return float(celsius) * 9 / 5 + 32
 
 def convert_to_celsius(fahrenheit):
     celsisus = (float(fahrenheit) - 32) * 5/9
