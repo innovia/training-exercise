f8eee345c1d038168bcbae235a987ef1949ed53d
Ami Mahloof <ami@lightricks.com>
2016-06-20 12:58:44 +0300

    fixes to 01-fundamentals

---
+++ 01-fundamentals/first_letter.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2016 Ami . All rights reserved.
+
+def get_user_password():
+    password = input("Tell me your password:")
+    return password
+
+
+def show_first_letter_of(password):
+    first_letter = get_first_letter_of(password)
+    print("The first letter you entered was: " + first_letter)
+
+def get_first_letter_of(text):
+    return text[0].upper()
+
+def main():
+    password = get_user_password()
+    show_first_letter_of(password)
+
+if __name__ == '__main__':
+    main()
