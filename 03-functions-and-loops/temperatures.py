9cd0159399e255c9157e80465bc8fbb55f786236
Ami Mahloof <ami@lightricks.com>
2016-06-20 13:59:15 +0300

    convert temperatures

---
+++ 03-functions-and-loops/temperatures.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2016 Ami . All rights reserved
+
+def convert_to_fahrenheit(celsius):
+    fahrenheit = float(celsius) * 9/5 + 32
+    return fahrenheit
+
+def convert_to_celsius(fahrenheit):
+    celsisus = (float(fahrenheit) - 32) * 5/9
+    return celsisus
+
