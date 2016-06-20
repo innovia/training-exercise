e659c51ea1b9c97ec0a98be67d1e28478035b49f
Ami Mahloof <ami@lightricks.com>
2016-06-20 15:31:47 +0300

    03-invest

---
+++ 03-functions-and-loops/invest.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2016 Ami . All rights reserved
+
+def invest(amount, rate, num_of_years):
+    initial_amount = amount
+    print("\nPrincipal amount: ", amount)
+
+    print("annual return rate: ", str(rate))
+
+    for year in range(1, num_of_years + 1):
+        compound_rate = amount * rate
+        amount = amount + compound_rate
+        print("year ", num_of_years, ":", amount)
+
+    roi  = amount - initial_amount
+    print("you made in {} years ${}".format(num_of_years, roi))
+
+def main():
+    invest(100, .05, 8)
+    invest(2000, .025, 5)
+
+if __name__ == '__main__':
+    main()
