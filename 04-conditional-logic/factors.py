b778f13bc272a2e2f4c904d53388941c1043ab8a
Ami Mahloof <ami@lightricks.com>
2016-06-20 16:08:36 +0300

    04-conditionals-logic

---
+++ 04-conditional-logic/factors.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2016 Ami . All rights reserved
+
+def get_a_positive_integer_from_user():
+    response = int(input("Enter a positive integer: "))
+
+    if not response:
+        get_a_positive_integer_from_user()
+
+    if response < 0:
+        print("You must provide a positive integer, try again...")
+        get_a_positive_integer_from_user()
+
+    return response
+
+def find_divusor_from_integer(integer):
+    for i in range(1, integer + 1):
+        if integer % i <= 0:
+            print(i, "is a divisor of", integer)
+
+def main():
+    num = get_a_positive_integer_from_user()
+    find_divusor_from_integer(num)
+
+if __name__ == '__main__':
+    main()
