d550d3162435199310284b4be19ff4514e63d01b
Ami Mahloof <ami@lightricks.com>
2016-06-21 16:28:59 +0300

    call function invest with keywords

--- 03-functions-and-loops/invest.py
+++ 03-functions-and-loops/invest.py
@@ -1,23 +1,22 @@
 # Copyright (c) 2016 Ami . All rights reserved
 
-def invest(amount, interest_rate, duration):
-    initial_amount = amount
+def invest(initial_amount, interest_rate, duration):
 
-    print("\nPrincipal amount: ", amount)
+    print("\nPrincipal amount: ", initial_amount)
 
     print("Annual return rate: ", interest_rate)
 
     for year in range(1, duration + 1):
-        compound_rate = amount * interest_rate
-        amount = amount + compound_rate
+        compound_rate = initial_amount * interest_rate
+        amount = initial_amount + compound_rate
         print("Year ", duration, ":", amount)
 
     roi  = amount - initial_amount
     print("You made in {} years ${}".format(duration, roi))
 
 def main():
-    invest(100, .05, 8)
-    invest(2000, .025, 5)
+    invest(initial_amount=100, interest_rate=.05, duration=8)
+    invest(initial_amount=2000, interest_rate=.025, duration=5)
 
 if __name__ == '__main__':
     main()
