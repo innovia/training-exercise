74a7162099f734368421c6ca353d7642b4070630
Ami Mahloof <ami@lightricks.com>
2016-06-21 16:51:48 +0300

    rename typo of devusor to devisor

--- 04-conditional-logic/factors.py
+++ 04-conditional-logic/factors.py
@@ -6,20 +6,16 @@ def get_a_positive_integer_from_user():
     if not response:
         get_a_positive_integer_from_user()
 
-    if response < 0:
-        print("You must provide a positive integer, try again...")
-        get_a_positive_integer_from_user()
-
     return response
 
-def find_divusor_from_integer(integer):
+def find_divisor_from_integer(integer):
     for i in range(1, integer + 1):
         if integer % i <= 0:
             print(i, "is a divisor of", integer)
 
 def main():
     num = get_a_positive_integer_from_user()
-    find_divusor_from_integer(num)
+    find_divisor_from_integer(num)
 
 if __name__ == '__main__':
     main()
