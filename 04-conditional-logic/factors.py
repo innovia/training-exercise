c2dec06d8e49cd8751a12484c454d0156e77951c
Ami Mahloof <ami@lightricks.com>
2016-06-22 10:40:33 +0300

    rename find_devisor function to print_devisors_of_integer

--- 04-conditional-logic/factors.py
+++ 04-conditional-logic/factors.py
@@ -8,14 +8,14 @@ def get_a_positive_integer_from_user():
 
     return response
 
-def find_divisor_from_integer(integer):
+def print_divisors_of_integer(integer):
     for i in range(1, integer + 1):
         if integer % i == 0:
             print(i, "is a divisor of", integer)
 
 def main():
     num = get_a_positive_integer_from_user()
-    find_divisor_from_integer(num)
+    print_divisors_of_integer(num)
 
 if __name__ == '__main__':
     main()
