769c55e787fa74ff7e86f8a1b448a82287f08f04
Ami Mahloof <ami@lightricks.com>
2016-06-23 10:16:16 +0300

    capitals assignment

---
+++ 05-lists-and-dictionaries/capital_city_loop.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2016 Ami . All rights reserved
+
+from capitals import capitals_dict
+from random import choice
+
+def get_random_state():
+    return choice(list(capitals_dict.keys()))
+
+def capital_of_state_quiz(state):
+    while True:
+        response = input("What is the capital of " + state + "? ").lower()
+
+        if response == capitals_dict[state].lower():
+            print("Correct")
+            return True
+        elif response == "exit":
+            print("Goodbye")
+            exit(0)
+        else:
+            print("Wrong - try again or type exit to quit")
+            return capital_of_state_quiz(state)
+
+def main():
+    state = get_random_state()
+    capital_of_state_quiz(state)
+
+if __name__ == '__main__':
+    main()
---
+++ 05-lists-and-dictionaries/capitals.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2016 Ami . All rights reserved
+
+capitals_dict = {
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Connecticut': 'Hartford',
+    'Delaware': 'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta'
+}
