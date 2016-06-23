e46f704278a3aa1df0088971d7a67169b3a61c8a
Ami Mahloof <ami@lightricks.com>
2016-06-23 11:05:15 +0300

    init commit

---
+++ 06-files-input-and-output/remove_files.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2016 Ami . All rights reserved
+
+import os
+
+def
+
+def main():
+    find_files_in_folder(folder, extention, file_max_size)
+
+if __name__ == '__main__':
+    main()
+
+
