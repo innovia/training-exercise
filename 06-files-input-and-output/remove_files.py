bbbfc80f31a15663e1f89d1256ae110764d2fdc2
Ami Mahloof <ami@lightricks.com>
2016-06-30 14:21:56 +0300

    rename full_path to pictures_full_path

--- 06-files-input-and-output/remove_files.py
+++ 06-files-input-and-output/remove_files.py
@@ -25,17 +25,17 @@ def find_files_in_folder(extension, file_max_size, path,):
 def main():
     pictures_folder_name = "little pics"
     script_folder = os.path.dirname(__file__)
-    full_path = os.path.join(script_folder, pictures_folder_name)
+    pictures_full_path = os.path.join(script_folder, pictures_folder_name)
 
-    if not os.path.exists(full_path):
-        print("Could not find the folder at " + full_path)
+    if not os.path.exists(pictures_full_path):
+        print("Could not find the folder at " + pictures_full_path)
         exit(1)
 
-    file_list = find_files_in_folder(path=full_path, extension="jpg", file_max_size=2000)
+    file_list = find_files_in_folder(path=pictures_full_path, extension="jpg", file_max_size=2000)
 
     if file_list:
         for file_name in file_list:
-            file_to_be_removed = os.path.join(full_path, file_name)
+            file_to_be_removed = os.path.join(pictures_full_path, file_name)
             print("Deleting {}...".format(file_to_be_removed))
             os.remove(file_to_be_removed)
     else:
